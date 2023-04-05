2950a7b9cc923207040506bb72d219302a38ff93
Alexander Kiko <elkikos@gmail.com>
2023-04-05 23:33:52 +0200

    Going HAM ITERATION 2 BEBE

---
+++ Models/CardinalDirection.cs
@@ -0,0 +1,12 @@
+public enum CardinalDirection
+{
+    North = 0,
+    South = 4,
+    East = 6,
+    West = 2,
+    Northeast = 7,
+    Northwest = 1,
+    Southeast = 5,
+    Southwest = 3,
+    Undefined = -1
+}

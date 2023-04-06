2e39c3dc60ca427dc509679e4806e1f8e09fa5b7
Alexander Kiko <elkikos@gmail.com>
2023-04-06 16:24:48 +0200

    rename

--- Models/ResponseObjects/WeatherRouteResponseObject.cs
+++ Models/ResponseObjects/WeatherRouteResponseObject.cs
@@ -7,6 +7,6 @@ public class WeatherRouteResponse
     public GeoCoordinate CoordinatesDestination { get; set; }
     public DateTime StartTime { get; set; }
     public DateTime FinishTime { get; set; }
-    public List<GeoWeather> GeoWeatherObjects { get; set; }
+    public List<WeatherRoutePoint> WeatherRoutePoints { get; set; }
     public string PolyLine { get; set; }
 }

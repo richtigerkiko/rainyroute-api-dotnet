b03b2223f200ff6e94b7cb792c022d88092cdb75
Alexander Kiko <elkikos@gmail.com>
2023-04-06 17:40:20 +0200

    adding function to lower resolution

--- Services/RouteServices.cs
+++ Services/RouteServices.cs
@@ -1,5 +1,6 @@
 using rainyroute.Models;
 using rainyroute.Models.ResponseObjects;
+using rainyroute.Models.ResponseObjects.ExternalApiResponses.OSRMApi;
 
 namespace rainyroute.Services;
 
@@ -34,8 +35,49 @@ public class RouteServices : BaseService
             return new WeatherRouteResponse(start, destination);
         }
 
+        // We expect only one Route and only one leg, so we can ignore everything else and simplyify the variable
+        var annotation = osrmRouteResponse.Routes[0].Legs[0].Annotation;
 
 
         throw new NotImplementedException();
+    }
+
+    private Annotation LowerAnnotationResolution(Annotation originalAnnotation, double resolutionMeters)
+    {
+        var resultObj = new Annotation();
+        resultObj.Metadata = originalAnnotation.Metadata;
+
+        // clear distance, duration and node List and leave first node
+        resultObj.Distance = new List<double>();
+        resultObj.Duration = new List<double>();
+        resultObj.Nodes = new List<object>() { originalAnnotation.Nodes[0] };
+
+        var distanceCounter = 0.0;
+        var durationCounter = 0.0;
+
+        // add 0 for first
+        resultObj.Distance.Add(0);
+        resultObj.Duration.Add(0);
+
+        // iterate through all nodes
+        for (int i = 0; i < originalAnnotation.Nodes.Count - 1; i++)
+        {
+            // Adding distancecounter
+            distanceCounter += originalAnnotation.Distance[i];
+            durationCounter += originalAnnotation.Duration[i];
+
+            // distancecounter is as big as resolutioncounter, add to returnobject
+            if (distanceCounter >= resolutionMeters && i >= originalAnnotation.Nodes.Count - 2)
+            {
+                resultObj.Distance.Add(distanceCounter);
+                resultObj.Duration.Add(durationCounter);
+                resultObj.Nodes.Add(originalAnnotation.Nodes[i]);
+
+                distanceCounter = 0.0;
+                durationCounter = 0.0;
+            }
+        }
+
+        return resultObj;
     }
 }
